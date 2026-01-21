2336ea13339698db1dcac8874b4292b3fcf752a2
Gabriele Di Muccio <gabrieledimuccio@gmail.com>
2026-01-21 13:12:39 +0100

    terminato selfwork cicli2

--- main2.js
+++ main2.js
@@ -1,2 +1,12 @@
-
+for (let i = 1; i < 20; i++){
+    i = i + 1;
+    console.log(i);
+}
+ let tot = 0
+for (let i = 0; i < 19; i++) {
+   i = i + 1;
+   tot += i;
+}
+ let media = tot / 10;
+  console.log(`La media dei numeri dispari è uguale a: ${media}`);
   
