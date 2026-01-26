09e17147fa63b2236d5a03c116f83940f74dac31
Gabriele Di Muccio <gabrieledimuccio@gmail.com>
2026-01-26 19:00:51 +0100

    terminato selfwork cicli 3

--- main3.js
+++ main3.js
@@ -1,1 +1,50 @@
-console.log(`Ciaone`);
+// SOLO NUMERI
+
+let scegli_bevanda;
+ do {
+     scegli_bevanda = Number(prompt(`Seleziona una bevanda : 1/Acqua, 2/Coca cola, 3/Birra.`))
+    
+     switch (scegli_bevanda) {
+         case 1:
+         console.log(`È stata selezionata l’acqua`);
+         break;
+         case 2:
+         console.log(`È stata selezionato coca cola`);
+         break;
+         case 3:
+         console.log(`È stata selezionata birra`);
+         break;
+         default:
+         console.log(`Scelta non valida. Seleziona una bevanda : 1/Acqua, 2/Coca cola, 3/Birra.`);      
+     }
+ }while(scegli_bevanda < 1 || scegli_bevanda > 3);
+
+ console.log(`Erogazione in corso.`);
+
+
+
+// NUMERI E STRINGHE
+do {
+     scegli_bevanda = prompt(`Seleziona una bevanda : 1/Acqua, 2/Coca cola, 3/Birra.`);
+    
+     switch (scegli_bevanda) {
+         case`1`:
+         case `acqua`:
+         console.log(`È stata selezionata l’acqua`);
+         break;
+         case `2`:
+         case `coca cola`:
+         console.log(`È stata selezionato coca cola`);
+         break;
+         case `3`:
+         case `birra`:
+         console.log(`È stata selezionata birra`);
+         break;
+         default:
+         console.log(`Scelta non valida. Seleziona una bevanda : 1/Acqua, 2/Coca cola, 3/Birra.`);      
+     }
+ }while(scegli_bevanda < 1 || scegli_bevanda > 3);
+
+ console.log(`Erogazione in corso.`);
+
+
